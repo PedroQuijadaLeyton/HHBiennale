4e8bf14cb144ee0efd632ff2543cfb2454aa9b09
PedroQuijadaLeyton <pedro@quiley.com>
2018-08-26 14:03:38 +0000

    init. main functionality of map controller implemented - zoom and pan

---
+++ Assets/Script/MapController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    [SerializeField] private GameObject bankDetail;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+	}
+
+    public void OnBankClicked()
+    {
+        bankDetail.SetActive(!bankDetail.activeSelf);
+    }
+
+}
